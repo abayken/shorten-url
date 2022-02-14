24f013970d36320723443de723bf716bf2173ef3
Abay Kenzhekhan <kenzhekhan@kolesa.kz>
2022-02-14 12:27:23 +0600

    написал тест на пост запрос

---
+++ internal/app/handlers/url_test.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/abayken/shorten-url/internal/app/storage"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+/// Тест который проверяет сокращения урла
+func TestURLSave(testing *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("https://hello.com/23213213123"))
+	recorder := httptest.NewRecorder()
+	customURLHandler := URLHandler{Storage: storage.MapURLStorage{}}
+	h := http.HandlerFunc(customURLHandler.ServerHTTP)
+	h.ServeHTTP(recorder, request)
+	result := recorder.Result()
+
+	assert.Equal(testing, 201, result.StatusCode)
+
+	bodyResult, err := ioutil.ReadAll(result.Body)
+	require.NoError(testing, err)
+	err = result.Body.Close()
+	require.NoError(testing, err)
+
+	shortURL := string(bodyResult[:])
+	assert.True(testing, shortURL != "")
+}
