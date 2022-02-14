2700cd65f894075b429336ba035b18b51f8edfee
Abay Kenzhekhan <kenzhekhan@kolesa.kz>
2022-02-15 00:25:24 +0600

    Добавил документацию; тесты не запускаются :(

--- internal/app/storage/url_storage.go
+++ internal/app/storage/url_storage.go
@@ -1,5 +1,7 @@
 package storage
 
+/// Абстракция storage-a
+/// Есть фейковая имлементация которая сохраняет в словарь
 type URLStorage interface {
 	Save(shortURLID, fullURL string)
 	Get(shortURLID string) string
