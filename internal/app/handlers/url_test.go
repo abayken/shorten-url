8bf6c9a6a1e06a35b13d98b29927e29b9e43c4ac
Abay Kenzhekhan <kenzhekhan@kolesa.kz>
2022-02-14 17:54:38 +0600

    Написал тест на гет метод

--- internal/app/handlers/url_test.go
+++ internal/app/handlers/url_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-/// Тест который проверяет сокращения урла
+/// Тест который проверяет сокращения урла через POST запрос
 func TestURLSave(testing *testing.T) {
 	request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("https://hello.com/23213213123"))
 	recorder := httptest.NewRecorder()
@@ -30,4 +30,27 @@ func TestURLSave(testing *testing.T) {
 
 	shortURL := string(bodyResult[:])
 	assert.True(testing, shortURL != "")
+}
+
+func TestURLGet(testing *testing.T) {
+	fullURL := "https://hello.com/23213213123"
+	request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(fullURL))
+	recorder := httptest.NewRecorder()
+	customURLHandler := URLHandler{Storage: storage.MapURLStorage{}}
+	h := http.HandlerFunc(customURLHandler.ServerHTTP)
+	h.ServeHTTP(recorder, request)
+	result := recorder.Result()
+
+	bodyResult, err := ioutil.ReadAll(result.Body)
+	require.NoError(testing, err)
+	err = result.Body.Close()
+	require.NoError(testing, err)
+
+	shortURL := string(bodyResult[:])
+
+	request = httptest.NewRequest(http.MethodGet, shortURL, nil)
+	getMethodRecorder := httptest.NewRecorder()
+	h.ServeHTTP(getMethodRecorder, request)
+	getMethodResult := getMethodRecorder.Result()
+	assert.Equal(testing, fullURL, getMethodResult.Header.Get("Location"))
 }
