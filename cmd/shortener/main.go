3d82aa67c086ce9ee407538a2e170f2d48d1694e
Abay Kenzhekhan <kenzhekhan@kolesa.kz>
2022-03-02 23:04:47 +0600

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"urls.json"`
 }
@@ -31,15 +31,7 @@ func main() {
 
 	flag.Parse()
 
-	var storageStrategy storage.URLStorage
-
-	if cfg.FileStoragePath == "" {
-		storageStrategy = storage.NewMapURLStorage(make(map[string]string))
-	} else {
-		storageStrategy = storage.FileURLStorage{Path: cfg.FileStoragePath}
-	}
-
-	router := GetRouter(storageStrategy, app.RealURLShortener{}, cfg)
+	router := GetRouter(storage.FileURLStorage{Path: cfg.FileStoragePath}, app.RealURLShortener{}, cfg)
 	router.Run(cfg.ServerAddress)
 }
 
